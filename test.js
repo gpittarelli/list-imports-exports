752d15d6de9536f4dc5486a3df6c7229144678b7
George Pittarelli <g@gjp.cc>
2019-03-28 17:28:43 -0700

    Make test script actually check the output

--- test.js
+++ test.js
@@ -1,13 +1,26 @@
-const parse = require('./').parse;
+const { parse } = require("./");
+const { deepEqual } = require("assert");
 
-console.log(
+deepEqual(
   parse(
     'import "babel-polyfill";' +
-    'import a from "./a";' +
-    'const c = 1; export default c; export {c};' +
-    'export b from "./b";' +
-    'export e from "./e";' +
-    'export {f} from "./e";' +
-    'export i, {g, h} from "./e";'
-  )
+      'import a from "./a";' +
+      'const c = 1; export default c; export {c};' +
+      'export b from "./b";' +
+      'export e from "./e";' +
+      'export {f} from "./e";' +
+      'export i, {g, h} from "./e";'
+  ),
+  {
+    imports: ["babel-polyfill", "./a", "./b", "./e", "./e", "./e"],
+    exports: ["default", "c", "b", "e", "f", "i", "g", "h"],
+    passthroughs: [
+      ["b", "./b", "default"],
+      ["e", "./e", "default"],
+      ["f", "./e", "f"],
+      ["i", "./e", "default"],
+      ["g", "./e", "g"],
+      ["h", "./e", "h"]
+    ]
+  }
 );
